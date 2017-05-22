626e82352cd4b45dde544af05cf35660b6249b53
Kyle Holohan <kholohan@gmail.com>
2017-05-21 21:43:16 -0400

    - Added plane class and tests

--- plane.py
+++ plane.py
@@ -3,12 +3,12 @@
 
 from vector import Vector
 
-class Line(object):
+class Plane(object):
 
     NO_NONZERO_ELTS_FOUND_MSG = 'No nonzero elements found'
 
     def __init__(self, normal_vector=None, constant_term=None):
-        self.dimension = 2
+        self.dimension = 3
 
         if not normal_vector:
             all_zeros = [0]*self.dimension
@@ -28,20 +28,20 @@ class Line(object):
             c = self.constant_term
             basepoint_coords = [0]*self.dimension
 
-            initial_index = Line.first_nonzero_index(n)
+            initial_index = Plane.first_nonzero_index(n)
             initial_coefficient = n[initial_index]
 
-            basepoint_coords[initial_index] = c / initial_coefficient
+            basepoint_coords[initial_index] = c/initial_coefficient
             self.basepoint = Vector(basepoint_coords)
 
         except Exception as e:
-            if str(e) == Line.NO_NONZERO_ELTS_FOUND_MSG:
+            if str(e) == Plane.NO_NONZERO_ELTS_FOUND_MSG:
                 self.basepoint = None
             else:
                 raise e
 
-    def __eq__(self, l):
-        basepoint_diff = self.basepoint - l.basepoint
+    def __eq__(self, p):
+        basepoint_diff = self.basepoint - p.basepoint
         return basepoint_diff.is_orthogonal(self.normal_vector)
 
     def __str__(self):
@@ -71,7 +71,7 @@ class Line(object):
         n = self.normal_vector
 
         try:
-            initial_index = Line.first_nonzero_index(n)
+            initial_index = Plane.first_nonzero_index(n)
             terms = [write_coefficient(n[i], is_initial_term=(i==initial_index)) + 'x_{}'.format(i+1)
                      for i in range(self.dimension) if round(n[i], num_decimal_places) != 0]
             output = ' '.join(terms)
@@ -89,27 +89,12 @@ class Line(object):
 
         return output
 
-    def intersection(self, l, tolerance=1e-10):
-        if self == l : return self.normal_vector #Lines are the same
-        elif self.is_parallel(l) : return None
-
-        denominator = (self.normal_vector[0] * l.normal_vector[1]) - (self.normal_vector[1] * l.normal_vector[0]) #AD-BC (constants of both lines)
-        x = ((l.normal_vector[1] * self.constant_term) - (self.normal_vector[1] * l.constant_term)) / denominator # (Dk_1 - Bk_2) / (AD - BC)
-        y = ((self.normal_vector[0] * l.constant_term) - (l.normal_vector[0] * self.constant_term)) / denominator # (Ak_2 + Ck_1) / (AD - BC)
-
-        return Vector([x, y])
-
-    def is_orthogonal(self, l):
-        return self.normal_vector.is_orthogonal(l.normal_vectorm)
-
-    def is_parallel(self, l, tolerance = 1e-10):
-        return self.normal_vector.is_parallel(l.normal_vector, tolerance)
-
-
+    def is_parallel(self, p, tolerance=1e-10):
+        return self.normal_vector.is_parallel(p.normal_vector, tolerance)
 
     @staticmethod
-    def first_nonzero_index(iterable):
+    def first_nonzero_index(iterable, tolerance=1e-10):
         for k, item in enumerate(iterable):
-            if not abs(item) < 1e-10:
+            if not abs(item) < tolerance:
                 return k
-        raise Exception(Line.NO_NONZERO_ELTS_FOUND_MSG)
+        raise Exception(Plane.NO_NONZERO_ELTS_FOUND_MSG)
---
+++ test/testPlane.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+# Author: github.com/kholohan
+
+import unittest
+
+from vector import Vector
+from plane import Plane
+
+class PlaneTest(unittest.TestCase):
+
+    def test_is_equal(self):
+        # plane 1: -0.412x + 3.806y + 0.728z = -3.46
+        # plane 2: 1.03x + 9.515y - 1.82z = 8.65
+        plane1 = Plane(Vector([-0.412, 3.806, 0.728]), -3.46)
+        plane2 = Plane(Vector([1.03, 9.515, -1.82]), 8.65)
+
+        result = plane1 == plane2
+        self.assertTrue(result)
+
+    def test_is_equal2(self):
+        # plane 1: 2.611x + 5.528y + 0.283z = 4.6
+        # plane 2: 7.715x + 8.306y + 5.342z = 3.76
+        plane1 = Plane(Vector([2.611, 5.528, 0.283]), 4.6)
+        plane2 = Plane(Vector([7.715, 8.306, 5.342]), 3.76)
+
+        result = plane1 == plane2
+        self.assertFalse(result)
+
+    def test_is_equal3(self):
+        # plane 1: -7.926x + 8.625y - 7.217 z = -7.952
+        # plane 2: -2.642x + 2.875y - 2.404z = -2.443
+        plane1 = Plane(Vector([-7.926, 8.625, -7.217]), -7.952)
+        plane2 = Plane(Vector([-2.642, 2.875, -2.404]), -2.443)
+
+        result = plane1 == plane2
+        self.assertFalse(result)
+
+    def test_is_parallel(self):
+        # plane 1: -0.412x + 3.806y + 0.728z = -3.46
+        # plane 2: 1.03x + 9.515y - 1.82z = 8.65
+        plane1 = Plane(Vector([-0.412, 3.806, 0.728]), -3.46)
+        plane2 = Plane(Vector([1.03, 9.515, -1.82]), 8.65)
+
+        result = plane1.is_parallel(plane2)
+        self.assertFalse(result)
+
+    def test_is_parallel2(self):
+        # plane 1: 2.611x + 5.528y + 0.283z = 4.6
+        # plane 2: 7.715x + 8.306y + 5.342z = 3.76
+        plane1 = Plane(Vector([2.611, 5.528, 0.283]), 4.6)
+        plane2 = Plane(Vector([7.715, 8.306, 5.342]), 3.76)
+
+        result = plane1.is_parallel(plane2)
+        self.assertFalse(result)
+
+    def test_is_parallel3(self):
+        # plane 1: -7.926x + 8.625y - 7.217 z = -7.952
+        # plane 2: -2.642x + 2.875y - 2.404z = -2.443
+        plane1 = Plane(Vector([-7.926, 8.625, -7.217]), -7.952)
+        plane2 = Plane(Vector([-2.642, 2.875, -2.404]), -2.443)
+
+        result = plane1.is_parallel(plane2, 0.1)
+        self.assertTrue(result)
